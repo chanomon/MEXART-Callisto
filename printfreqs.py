603a33e87e5ad4ad7d21f1acc8ec07a254c35939
chanomon <hdomeli@gmail.com>
2018-03-12 13:40:32 -0600

    Update printfreqs.py
    made function to be called, instead of running it separatedly

--- printfreqs.py
+++ printfreqs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-
+#This program prints frecuencies of each list index in of the frequencies list, usefull for the analysis.
 from astropy.io import fits
 ###########Este programa imprime las frecuencias con el numero de los canales
+def printfreqs(fitpath):
+	
+	fl = fits.open(fitpath)#checar que evento se esta analizando
+	freqs = fl[1].data['frequency'][0]
 
-file_path = raw_input('nombre del fit  ')
-fl = fits.open( file_path)#checar que evento se esta analizando
-freqs = fl[1].data['frequency'][0]
-
-for i in range(len(freqs)):
-	print '%i %f'%(i,freqs[i])
+	for i in range(len(freqs)):
+		print '%i %f'%(i,freqs[i])
