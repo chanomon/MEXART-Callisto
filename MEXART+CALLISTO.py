52f71a91b1efeadfa02641917d110555bb2ca381
chanomon <hdomeli@gmail.com>
2018-03-13 20:07:23 -0600

    Update MEXART+CALLISTO.py
    program now runs without any bugs.

--- MEXART+CALLISTO.py
+++ MEXART+CALLISTO.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-#file_path=["MEXART_20151104_133000_59.fit"]
-#start='13:30'
-#end='13:45'
-#    if file.endswith("-0.TMSERIES.txt"):	#checar que canal se esta usando 
-#        mex_v.append(float(str_v)+5.6)#*10.0)
-
 import os
 from readcol import fgetcols 
 import numpy as np
@@ -20,7 +14,7 @@ from scipy.optimize import curve_fit
 from scipy import asarray as ar,exp
 import datetime as dt
 from dec_to_time import dec_to_time as dtt
-
+from read_cut import read_cut
 from datetime import timedelta
 
 
@@ -34,48 +28,20 @@ def movingaverage(interval, window_size):
 	        window = np.ones(int(window_size))/float(window_size) 
 	        return np.convolve(interval, window, 'same')
 
-###############################################################
-#Periodo de tiempo
-##############################################################
-path = './'		#checar path, #se tiene que correr en la carpeta contenedora de los archivos
-
-files = []
-'''
-start='16:00:50'
-end='16:14:10'
-
-hh_start, mm_start ,ss_start= start.split(':')
-hh_end, mm_end, ss_start = end.split(':')
-
-# los tiempos se pasan a horas decimales
-start_flag=float(hh_start)+ float(mm_start)/60.0 + float(ss_start)/3600.
-end_flag=float(hh_end)+ float(mm_end)/60.0 + float(ss_start)/3600.
-
-print 'start: ',start_flag
-print 'end:  ', end_flag
-'''
-	
-	
-
-
 #########################################################################
 ##parte de MEXART
 #########################################################################
-
-
-
 ######################################################################
 ##obtencion de la fecha a partir del nombre del archivo
 ######################################################################
+path = './'		#checar path, #se tiene que correr en la carpeta contenedora de los archivos
+files = []
 
-for file in os.listdir(path):
+for file in os.listdir('./'):
     if file.endswith("mexcut.dat"):	#checar que canal se esta usando 
         files.append(file)
 #print 'number of mexart files'+str(len(files))
 files= sorted(files) #se ordenan los archivos
-
-#fig = plt.figure()
-#ax = plt.subplot(111)
 
 stream_time = []
 stream_v = []
@@ -86,42 +52,12 @@ for file in files:
     m = (splits[1])
     d = (splits[2])
 
-######################################################################
-##Comentar a partir de aqui para no volver a recortar datos de MEXART
-######################################################################
-'''
-##################################################################
-## seccion que guarda el recorte de los datos en un archivo nuevo
-##################################################################
 
-    g = open(path+year+'-'+month+'-'+day+'-event.dat','w')
-    
-    with open(path+file, "rb") as f:
-        for i in range(10):
-            f.readline()
-        content = f.readlines()
-    print 'empezando a leer datos de mexart'    
-    stream_by_day_time=[]
-    stream_by_day_v=[]
-    for line in content:
-        str_time, str_v = line.split()
-        time = float(str_time)
-        v = float(str_v)
-        if start_flag <= time and time <= end_flag:
-            g.write(str_time +'\t'+ str_v+'\n')
-            stream_by_day_time.append(time)
-            stream_by_day_v.append(v)
-    #stream_time.append(stream_by_day_time)
-    #stream_v.append(stream_by_day_v)
-    g.close()
-    for i in range(len(stream_by_day_v)):
-    	stream_by_day_v[i]=(stream_by_day_v[i]+6.4)*3.0	#correccion de voltaje
-    plt.plot(stream_by_day_time,stream_by_day_v,linewidth=1.5,label='MEXART',color='r')
-#333333333333333333333333333333    
-#Termina parte que debe estar comentada
-#333333333333333333333333333333
-'''
-	
+readMEX=raw_input('para mexcut escribe y, si ya esta mexcut escribe n')
+if readMEX == 'y':
+        inicio = raw_input('escribe tiempo de inicio formato hh:mm')
+        fin = raw_input('escribe tiempo de fin formato hh:mm')
+        read_cut(inicio,fin)
 
 #############################################
 ##Abrir archivo recortado para graficarlo
@@ -153,18 +89,23 @@ for i in range(len(mex_time)):
 	else:	
 		delta = dt.timedelta(microseconds=(mex_time[i+1]-mex_time[i])*3600*1000000)
 		datetime.append(datetime[i]+delta)
-
+                
+#################################################################
 #Parte de Callisto
 #################################################################
+file_path=[]
+for file in os.listdir('./'):
+    if file.endswith(".fit"):
+            file_path.append(file)
 
 callisto=[]
 t_callisto=[]
-file_path=["MEXART_20180301_220000_59.fit"]
-for i in file_path:
+
+for f in file_path:
         
-	file_path=i#raw_input('Enter your input file   ')
+	#f_path=i#raw_input('Enter your input file   ')
 	
-	fl = fits.open( file_path)
+	fl = fits.open( f)
 	data = fl[0].data
 	axes = fl[1]
 	header = fl[0].header
@@ -196,33 +137,10 @@ for i in file_path:
         
 	for index in range(len(data[0])):
 		suma=0.0
-		#x_time[i]=x_time[i]#+0.07-0.0007	#correccion de tiempo en callisto
-		for j in range(143,183):#131,133 #(126,138)para ver frecuencias de puschino
+		for j in range(0,190):#131,133 #(126,138)para ver frecuencias de puschino
 			suma = suma + data[j][index]
-				#print "excluding"	
-		#light_curve.append(suma)#-284.0)  #se comenta esta linea para ahorrar un paso
-                callisto.append((suma/100 -286)*5+1090)   # se resta para poner la senal de Callisto a 0
+		callisto.append((suma/100 -286)*5+1090)   # se resta para poner la senal de Callisto a 0
                 t_callisto.append(x_time[index])
-
-
-################## rms de MEXART
-'''
-plt.plot(mex_v)
-plt.show()
-irms=int(raw_input('indice inicio rms	'))
-frms=int(raw_input('indice final rms	'))
-
-max_mex = max(mex_v)    
-max_mex_t = datetime[mex_v.index(max_mex)]
-rms_mex = np.std(mex_v[irms:frms])
-StoN = max_mex/rms_mex
-print 'el max de intensidad es:  ',max_mex
-print 'el tiempo del max es:  ',max_mex_t
-print 's/n es:	', StoN
-print 'el rms de mexart es:	', rms_mex    
-'''
-
-
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
@@ -232,17 +150,10 @@ plt.plot(datetime,mex_v,'b',label='MEXART',linewidth=1.5)
 #ax.xaxis.set_major_locator(dates.MinuteLocator(interval=3))
 ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M:%S'))
 plt.xticks(rotation=45)
-
-
-
 plt.legend()
 plt.title('MEXART + CALLISTO '+yyyy+'-'+m+'-'+d)
 plt.xlabel('UTC time')
 plt.ylabel('intensity')
 plt.subplots_adjust(bottom=.2)
 plt.grid()
-#fig.savefig("plot-mexart-data.eps", format="eps")
-
 plt.show()
-
-
